ff609e5e313fb7e994e24c43f2c7cd99d0d02c19
TonyTillet <tony@wagaia.com>
2023-03-21 19:34:07 +0100

    Compilation des assets + tailwind css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,12 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Formation </title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
 </head>
 <body>
     @include('partials.navbar')
     @yield('content')
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
